f35673c7e7aadb9223373679e2cde28c0f5f1db4
John Alexander <jalexander788@gmail.com>
2015-04-07 20:28:53 -0400

    adding jquery

---
+++ src/app/main/jquery.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    console.log("sanity check");
+//  $('a[href*=#]:not([href=#portfolioModal1], [href=#portfolioModal2], [href=#portfolioModal3], [href=#portfolioModal4], [href=#portfolioModal5], [href=#portfolioModal6])').click(function() {
+//    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+//      var target = $(this.hash);
+//      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//      if (target.length) {
+//        $('html,body').animate({
+//          scrollTop: target.offset().top
+//        }, 1000);
+//        return false;
+//      }
+//    }
+//  });
+});
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -25,19 +25,4 @@ angular.module('johnalxndrgithubio')
         angular.forEach($scope.awesomeThings, function (awesomeThing) {
             awesomeThing.rank = Math.random();
         });
-   
-$(function() {
-  $('a[href*=#]:not([href=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
      });
